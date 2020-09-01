9369a37a762263a853934eb88069ce0b1b1614bf
xie <yaxu0325@gmail.com>
2020-09-01 16:28:03 +0200

    upload mono splits

--- options.py
+++ options.py
@@ -34,7 +34,7 @@ class MonodepthOptions:
         self.parser.add_argument("--split",
                                  type=str,
                                  help="which training split to use",
-                                 choices=["eigen_zhou", "eigen_full", "odom", "benchmark", "rosario_stereo"],
+                                 choices=["eigen_zhou", "eigen_full", "odom", "benchmark", "rosario_stereo", "rosario_mono"],
                                  default="rosario_stereo")
         self.parser.add_argument("--num_layers",
                                  type=int,
@@ -45,18 +45,18 @@ class MonodepthOptions:
                                  type=str,
                                  help="dataset to train on",
                                  default="rosario_stereo",
-                                 choices=["kitti", "kitti_odom", "kitti_depth", "kitti_test", "rosario_stereo"])
+                                 choices=["kitti", "kitti_odom", "kitti_depth", "kitti_test", "rosario_stereo", "rosario_mono"])
         self.parser.add_argument("--png",
                                  help="if set, trains from raw KITTI png files (instead of jpgs)",
                                  action="store_true")
         self.parser.add_argument("--height",
                                  type=int,
                                  help="input image height",
-                                 default=192)
+                                 default=384)
         self.parser.add_argument("--width",
                                  type=int,
                                  help="input image width",
-                                 default=640)
+                                 default=672)
         self.parser.add_argument("--disparity_smoothness",
                                  type=float,
                                  help="disparity smoothness weight",
