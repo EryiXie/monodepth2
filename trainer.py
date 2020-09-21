fdc21ec5d264b21a072ae63a993f1ec523eb2694
yaxu <yaxu0325@gmail.com>
2020-09-21 19:28:24 +0200

    umm

--- trainer.py
+++ trainer.py
@@ -573,7 +573,7 @@ class Trainer:
                 "disp_{}/{}".format(s, j),
                 normalize_image(outputs[("disp", s)][j]), self.step)
 
-            histo = outputs["reprojection_losses/{}".format(s)].view(-1).histc(bins=100, min=0, max=1)
+            histo = torch.Tensor(outputs["reprojection_losses/{}".format(s)]).view(-1).histc(bins=100, min=0, max=1)
             writer.add_histogram( "reprojection_loss_{}/{}".format(s, j),
                                   histo, self.step)
 
