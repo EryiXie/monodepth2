538c30b4965f405ec064823d0632bf9aa8fb518c
EryiXie <yaxu0325@gmail.com>
2020-09-22 11:10:15 +0200

    add min reprojection mask

--- trainer.py
+++ trainer.py
@@ -481,8 +481,8 @@ class Trainer:
             else:
                 to_optimise, idxs = torch.min(combined, dim=1)
                 outputs["min_reprojection/{}".format(scale)] = idxs.float()/idxs.max()
-                print(scale,"combined",combined.shape)
-                print(scale,"idxs",idxs.shape)
+                print(scale, "combined", combined.shape)
+                print(scale, "idxs", idxs.shape, idxs.max())
 
             if not self.opt.disable_automasking:
                 outputs["identity_selection/{}".format(scale)] = (
